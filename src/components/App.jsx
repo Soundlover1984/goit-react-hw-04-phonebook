641b636da4af81ee350807b36b3ee083bed70e26
Soundlover1984 <denys.kovtun@gmail.com>
2023-07-19 20:30:02 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -12,82 +12,71 @@ import { useLocalStorage } from "./LocalStorage/LocalStorage";
 
 export const App = () => {
   
-
+  const [contacts, setContacts] = useLocalStorage('contacts', defaultContacts);
+  const [filter, setFilter] = useState('');
   
 
-  componentDidMount() {
-    const contactsFromStorage = JSON.parse(localStorage.getItem('contacts'));
-
-    if (contactsFromStorage) {
-      this.setState({ contacts: contactsFromStorage });
-
-      if (contactsFromStorage.length === 0) {
-        Notiflix.Notify.info('No contacts in your list yet');
-      }
-    }
-  }
-
-  componentDidUpdate(prevState) {
-    if (prevState.contacts !== this.state.contacts) {
-      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
-    }
-  }
-
-  onAddBtnClick = FormData => {
+  const onAddBtnClick = FormData => {
     const { name, number } = FormData;
-    const includesName = this.state.contacts.find(
+    const id = nanoid();
+
+    const includesName = contacts.find(
       contact => contact.name.toLocaleLowerCase() === name.toLocaleLowerCase()
     );
 
     if (includesName) {
-      return Notiflix.Notify.warning(`${name} is already in contacts`);
+      return Notiflix.Notify.warning(
+        `${name} is already in contacts`
+      );
     } else {
-      let contact = { id: nanoid(), name: name, number: number };
-      this.setState(prevState => ({
-        contacts: [...prevState.contacts, contact],
-      }));
+      const contact = { id, name, number };
+      setContacts(prevContacts => [...prevContacts, contact]);
       Notiflix.Notify.success(
         `${name} was successfully added to your contacts`
       );
     }
   };
 
-  onDeleteBtnClick = (id, name) => {
-    this.setState(prevState => ({
-      contacts: prevState.contacts.filter(contact => contact.id !== id),
-    }));
-    Notiflix.Notify.info(`${name} was successfully deleted from your contacts`);
+  const onDeleteBtnClick = (id, name) => {
+    setContacts(prevContacts =>
+      prevContacts.filter(contact => contact.id !== id)
+    );
+    Notiflix.Notify.info(
+      `${name} was successfully deleted from your contacts`
+    );
   };
 
-  onFilterChange = event => {
-    this.setState({ filter: event.target.value });
+  const onFilterChange = event => {
+    setFilter(event.target.value);
   };
 
-  filterContacts = () => {
-    const query = this.state.filter.toLocaleLowerCase();
+  const filterContacts = () => {
+    const query = filter.toLocaleLowerCase();
 
-    const filteredContacts = this.state.contacts.filter(contact =>
+    const filteredContacts = contacts.filter(contact =>
       contact.name.toLocaleLowerCase().includes(query)
     );
 
-    if (filteredContacts.length === 0) {
-      Notiflix.Notify.warning('No contacts matching your request');
+    if (query && !filteredContacts.length) {
+      Notiflix.Notify.warning(
+        'No contacts matching your request'
+      );
     }
+
     return filteredContacts;
   };
 
-  render() {
+ 
     return (
       <Container>
         <MainHeader>Phonebook</MainHeader>
-        <ContactForm onAddBtnClick={this.onAddBtnClick} />
+        <ContactForm onAddBtnClick={onAddBtnClick} />
         <SubHeader>Contacts</SubHeader>
-        <Filter value={this.state.filter} onChange={this.onFilterChange} />
+        <Filter value={filter} onChange={onFilterChange} />
         <ContactList
-          contacts={this.filterContacts()}
-          onDeleteBtnClick={this.onDeleteBtnClick}
+          contacts={filterContacts()}
+          onDeleteBtnClick={onDeleteBtnClick}
         />
       </Container>
     );
-  }
 };
